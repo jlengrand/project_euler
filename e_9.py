3b6ce5578f65e029ff007af28d19084e431eff02
Julien Lengrand-Lambert <julien@lengrand.fr>
2012-01-13 10:54:30 +0100

    Solves Problem 8 Prepares Problem 9
    Creates a bash script which will do templates for me

---
+++ e_9.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python 
+"""
+ ##---
+ # jlengrand
+ #Created on : Fri Jan 13 10:49:29 CET 2012
+ #
+ # DESCRIPTION : Solves problem 9 of Project Euler
+ A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
+
+ a^2 + b^2 = c^2
+ For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ Find the product abc.
+ ##---
+"""
+def fun():
+    """
+    """
+
+    return 1
+
+if __name__ == '__main__':
+    print "Answer : %d" % (fun())

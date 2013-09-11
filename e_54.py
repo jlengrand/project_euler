817c2b8683ccf4c2e4a206ec33f5260606caeb94
julien lengrand-lambert <julien@lengrand.fr>
2013-09-11 16:06:19 +0200

    Starts working on problem 54. Currently just loads the data in a way that can be used. Next step is to sort players cards

---
+++ e_54.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+'''
+Created on 11 sept. 2013
+
+@author: Julien Lengrand-Lambert
+
+DESCRIPTION: Solves problem 54 of Project Euler
+The file, poker.txt, contains one-thousand random hands dealt to two players.
+Each line of the file contains ten cards (separated by a single space):
+the first five are Player 1's cards and the last five are Player 2's cards.
+You can assume that all hands are valid (no invalid characters or repeated
+    cards), each player's hand is in no specific order, and in each hand there
+is a clear winner.
+
+How many hands does Player 1 win?
+'''
+
+def load_data(filename):
+    """
+    Loads the given file according to the problem description.
+    Returns a list of two hands of poker players.
+    """
+    cards_in_poker = 5
+    hands = []
+    file = open(filename, "r")
+
+    #for line in file :
+    for i in range(2):
+        res = file.readline().rstrip() # also removes eol
+        res_spl = res.split(" ")
+
+        # creating left and right player's hands
+        left = res_spl[:cards_in_poker]
+        right = res_spl[cards_in_poker:]
+
+        hands.append([left, right])
+
+    file.close()
+    return hands
+
+def winning_hands(filename):
+    """
+    Returns the first of the firs num consecutive primes.
+    """
+    hands = load_data(filename)
+    for val in hands:
+        print val
+
+if __name__ == '__main__':
+    winning_hands("./e_54_poker.txt")
+    #print 1
+    #print "Answer : %d " % (winning_hands("./e_54_poker.txt"))
